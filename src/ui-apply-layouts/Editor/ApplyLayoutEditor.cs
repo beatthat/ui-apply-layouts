dd86f2b5260541095e5fa87f24d76820be0c05ee
larrykirschner <larrykirschner@gmail.com>
2017-09-12 17:18:42 -0700

    0.0.1

---
+++ src/ui-apply-layouts/Editor/ApplyLayoutEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace BeatThat
+{
+
+	public static class ApplyLayoutEditor : UnityEditor.Editor
+	{
+		[MenuItem("GameObject/UI/Ape - Layout - Apply (recursively)")]
+		public static void ApplyRecursive(MenuCommand cmd)
+		{
+			var go = cmd.context as GameObject;
+			if(go == null) {
+				Debug.Log("Must be called with a scene object selected");
+				return;
+			}
+
+			var list = go.GetComponentsInChildren<IAppliesLayout>(true);
+			foreach(var item in list) {
+				item.ApplyLayout();
+			}
+		}
+	}
+}
---
+++ src/ui-apply-layouts/IAppliesLayout.cs
@@ -0,0 +1,12 @@
+﻿
+namespace BeatThat
+{
+	/// <summary>
+	/// Interface for components that apply a layout (e.g. sets rects, enables/disables components.)
+	/// Exists to enable an editor function to recursively apply layout from a layout-controller parent object.
+	/// </summary>
+	public interface IAppliesLayout 
+	{
+		void ApplyLayout();
+	}
+}
